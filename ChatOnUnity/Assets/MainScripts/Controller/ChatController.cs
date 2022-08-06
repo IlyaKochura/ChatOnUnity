d7b2e8e24c4dc0a5f8ec22f92f0abdec8e33c060
ilya Kochura <ikochura89@gmail.com>
2022-08-06 16:14:40 +0300

    Сделал запись ссобщений в чат

---
+++ ChatOnUnity/Assets/MainScripts/Controller/ChatController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using MainScripts.ButtonScripts;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MainScripts.Controller
+{
+    public class ChatController : MonoBehaviour
+    {
+        [Header("Кнопки")] 
+        [SerializeField] private ButtonScript butonSend;
+        [SerializeField] private ButtonScript buttonDelete;
+        [SerializeField] private TMP_InputField inputField;
+
+        private List<string> _messageList = new();
+        
+
+        void Start()
+        {
+            butonSend.Action += SendMessageInChat;
+        }
+
+
+        void Update()
+        {
+            buttonDelete.Action += SelectMessageInChat;
+        }
+
+
+        private void SendMessageInChat()
+        {
+            var text = inputField.text;
+            inputField.text = "";
+            inputField.Select();
+            _messageList.Add(text);
+            for (int i = 0; i < _messageList.Count; i++)
+            {
+                Debug.LogError(_messageList[i]);
+            }
+        }
+
+        private void SelectMessageInChat()
+        {
+            
+        }
+    }
+}
